6b96801cd8a07f61750c20cf6b73f82b42dd3478
eldho-paul <eldhosemp88@gmail.com>
2021-02-01 22:37:28 +0530

    updating project

---
+++ src/test/java/com/eldho/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eldho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
